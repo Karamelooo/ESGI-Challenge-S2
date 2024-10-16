cc45ace7e0dd4f913a274ffec305b29552049778
leo P <Theoasis953@gmail.com>
2024-10-16 13:18:03 +0200

    feat: :sparkles: eslint in backend

--- apps/backEnd/src/index.ts
+++ apps/backEnd/src/index.ts
@@ -1,20 +1,21 @@
-import 'dotenv/config';
-import express, { Application } from 'express';
-import mongoose from 'mongoose';
+import type { Application } from 'express'
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
 
 // const mongoString = process.env.DB_URL;
-const mongoString = "mongodb://esgi:esgi@database:27017";
+const mongoString = 'mongodb://esgi:esgi@database:27017'
 
 mongoose.connect(mongoString).then(() => {
-  console.log('Database Connected');
+  console.log('Database Connected')
 }).catch((error: Error) => {
-  console.error(error);
-});
+  console.error(error)
+})
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(express.json());
+app.use(express.json())
 
 app.listen(8080, () => {
-  console.log("Server started and listening on port 8080");
-});
+  console.log('Server started and listening on port 8080')
+})
