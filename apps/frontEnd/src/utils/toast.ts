073378e7c04d2ebfd96335398b81e5c6b70684c0
Hugo PETIT <hugo.petitleneuder@gmail.com>
2024-11-21 18:19:08 +0100

    toast js to ts

--- apps/frontEnd/src/utils/toast.ts
+++ apps/frontEnd/src/utils/toast.ts
@@ -1,6 +1,6 @@
 import Toast from 'typescript-toastify'
 
-export function showToast(message) {
+export function showToast(message: string): void {
   const toast = new Toast({
     position: 'top-right',
     autoCloseTime: 2000,
