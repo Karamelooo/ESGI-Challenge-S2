6ae652e70d972ad312daa8ff7cd35510dcc92b53
lbouzar <lyzabouzar@gmail.com>
2024-12-11 15:33:40 +0100

    test signature

--- apps/frontEnd/src/services/stripeService.ts
+++ apps/frontEnd/src/services/stripeService.ts
@@ -1,5 +1,5 @@
 import { loadStripe, type Stripe } from '@stripe/stripe-js';
 
-const stripePromise: Promise<Stripe | null> = loadStripe('your-publishable-key'); // Replace with your Stripe publishable key
+const stripePromise: Promise<Stripe | null> = loadStripe('your-publishable-key'); // key à ajouter
 
 export default stripePromise;
